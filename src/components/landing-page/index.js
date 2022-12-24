d7d6694a3781d94761c0015d73724f4021d20acd
seluciano <luciano.seibel.freelancer@gmail.com>
2022-12-24 13:30:53 +0200

    clean

--- src/components/landing-page/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LandingCompress } from './LandingCompress';
-export { default as LandingInformation } from './LandingInformation';
-export { default as LandingFaq } from './LandingFaq';
--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const CONSTANTS = {
-  // serverURL: 'http://164.92.68.61'
-  serverURL: 'http://localhost:3001'
-};
