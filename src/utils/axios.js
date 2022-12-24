53a459b0ff42b97cefe42d80fb1a38201708c54b
seluciano <luciano.seibel.freelancer@gmail.com>
2022-12-24 13:42:19 +0200

    update axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
-import { CONSTANTS } from './constants';
 
 // ----------------------------------------------------------------------
 
-const axiosInstance = axios.create({ baseURL: CONSTANTS.serverURL });
+const axiosInstance = axios.create();
 
 axiosInstance.interceptors.response.use(
   (response) => response,
-  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+  (error) =>
+    Promise.reject(
+      (error.response && error.response.data) || 'Something went wrong'
+    )
 );
 
 export default axiosInstance;
