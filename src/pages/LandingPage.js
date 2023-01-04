5eb08e0a1f7e01dae826e7c34de2099557d3fc6c
AgileSolution47 <agile.solutioner@gmail.com>
2023-01-04 12:04:12 -0300

    update PDF24 Tools website

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -21,7 +21,7 @@ const ContentStyle = styled('div')(({ theme }) => ({
 
 export default function LandingPage() {
   return (
-    <RootStyle title="PDF24 tools | Home" id="move_top">
+    <RootStyle title="PDF24 Tools | Home" id="move_top">
       <LandingTopbar />
       <ContentStyle>
         <LandingCompress />
